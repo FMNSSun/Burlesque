5513949ebcfd416ec3f76f3b48461510c890d598
Roman Muentener <fmnssun@gmail.com>
2012-07-25 10:07:33 +0200

    * fancy highlight

--- cgi.hs
+++ cgi.hs
@@ -8,11 +8,17 @@ import Burlesque.Display
 
 main = runCGI $ handleErrors cgiMain
 
+linkCSS
+ = thelink ! [rel "stylesheet", thetype "text/css", href "style.css"] << noHtml
+
+documentHtml content
+ = (header << ((thetitle << "Burlesque Shell") +++ linkCSS)) +++ (body << content)
+
 page q = do
- output . renderHtml $ body << ((form ! [method "get",action "burlesque.cgi"])
+ output . renderHtml $ documentHtml $ ((form ! [method "get",action "burlesque.cgi"])
      << (   (input ! [thetype "text", name "q"])
         +++ (input ! [thetype "submit", value "Go!"])
-        )) +++ (h1 << "Result") +++ (pre << q)
+        )) +++ (h1 << "Result") +++ (pre << (primHtml q))
 
 cgiMain = do
   mn <- getInput "q"
@@ -22,4 +28,4 @@ cgiMain = do
 
 runProgramNoStdin :: String -> String
 runProgramNoStdin p =
- unlines . map toDisplay $ execState (eval (runParserWithString parseBlsq p)) []
+ unlines . map toHTML $ execState (eval (runParserWithString parseBlsq p)) []
