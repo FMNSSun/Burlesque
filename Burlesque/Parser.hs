ba1848783d59385e6e49e7955a78ed469049f939
Roman Muentener <fmnssun@gmail.com>
2012-07-25 15:13:53 +0200

    * tidy parse code

--- Burlesque/Parser.hs
+++ Burlesque/Parser.hs
@@ -17,53 +17,60 @@ import Burlesque.Types
 import Debug.Trace
 
 parseDouble :: Parser BlsqExp
-parseDouble = do s <- many $ char '-'
-                 n1 <- many1 digit
-                 char '.'
-                 n2 <- many1 digit
-                 optional spaces
-                 if not.null $ s then 
-                    return $ BlsqDouble (-1 * (read (n1 ++ "." ++ n2)))
-                 else return $ BlsqDouble (read (n1 ++ "." ++ n2))
+parseDouble = do 
+  s <- many $ char '-'
+  n1 <- many1 digit
+  char '.'
+  n2 <- many1 digit
+  optional spaces
+  if not.null $ s then 
+    return $ BlsqDouble (-1 * (read (n1 ++ "." ++ n2)))
+  else return $ BlsqDouble (read (n1 ++ "." ++ n2))
 
 parseNumber :: Parser BlsqExp
-parseNumber = do s <- many $ char '-'
-                 num <- many1 digit
-                 optional spaces
-                 if not.null $ s then
-                   return $ BlsqInt (-1 * (read num))
-                 else return $ BlsqInt (read num)
+parseNumber = do 
+  s <- many $ char '-'
+  num <- many1 digit
+  optional spaces
+  if not.null $ s then
+    return $ BlsqInt (-1 * (read num))
+  else return $ BlsqInt (read num)
 
 parseChar :: Parser BlsqExp
-parseChar = do char '\''
-               c <- anyChar
-               optional spaces
-               return $ BlsqChar c
+parseChar = do 
+  char '\''
+  c <- anyChar
+  optional spaces
+  return $ BlsqChar c
 
 parseIdent :: Parser BlsqExp
-parseIdent = do a <- noneOf "1234567890{}',\" "
-                b <- noneOf "1234567890{}',\" "
-                optional spaces
-                return . BlsqIdent $ a:b:[]
+parseIdent = do 
+  a <- noneOf "1234567890{}',\" "
+  b <- noneOf "1234567890{}',\" "
+  optional spaces
+  return . BlsqIdent $ a:b:[]
 
 parseSep :: Parser BlsqExp
-parseSep = do b <- char ','
-              optional spaces
-              return $ BlsqSpecial [b]
+parseSep = do
+  b <- char ','
+  optional spaces
+  return $ BlsqSpecial [b]
 
 parseBlock :: Parser BlsqExp
-parseBlock = do s <- char '{'
-                e <- parseBlsq
-                t <- char '}'
-                optional spaces
-                return $ BlsqBlock e
+parseBlock = do
+  s <- char '{'
+  e <- parseBlsq
+  t <- char '}'
+  optional spaces
+  return $ BlsqBlock e
 
 parseString :: Parser BlsqExp
-parseString = do s <- char '"'
-                 e <- many (noneOf "\"")
-                 t <- char '"'
-                 optional spaces
-                 return $ BlsqStr e
+parseString = do 
+  s <- char '"'
+  e <- many (noneOf "\"")
+  t <- char '"'
+  optional spaces
+  return $ BlsqStr e
 
 parseBlsq :: Parser [BlsqExp]
 parseBlsq = many parseBlsq'
