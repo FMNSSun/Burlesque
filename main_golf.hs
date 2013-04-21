afc0a9bed9dbeec5239323d7e1eadf0bc5adaad6
Roman Muentener <roman.muentener@bluewin.ch>
2013-04-21 11:42:01 +0200

    + main_golf

---
+++ main_golf.hs
@@ -0,0 +1,24 @@
+import Burlesque.Parser
+import Burlesque.Types
+import Burlesque.Eval
+import Burlesque.Display
+
+import System.Environment
+import System.IO
+import Data.List
+
+runProgram :: String -> String -> String
+runProgram p stdin =
+ unlines . map toDisplay . filter notHidden $ execState (eval (runParserWithString parseBlsq p)) [BlsqStr stdin]
+
+
+main = do
+ args <- getArgs
+ case args of
+   ["--file",file] -> do 
+     prog <- readFile file
+     interact $ runProgram prog
+   _ -> do putStrLn $ "Invalid usage"
+           putStrLn "  --file <path>           Read code from file (incl. STDIN)"
+           putStrLn ""
+           putStrLn "\tBurlesque\tRoman Muentener, 2012"
