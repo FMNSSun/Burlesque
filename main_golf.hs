c939e90bde40363324d21b3eeb064693b8583799
mroman <me@mroman.ch>
2018-11-19 11:27:54 +0100

    ! main_golf.hs

--- main_golf.hs
+++ main_golf.hs
@@ -11,17 +11,19 @@ import Data.List
 
 import qualified Data.Map as M
 
-runProgram :: String -> String -> String
-runProgram p stdin =
- unlines . map toDisplay . filter notHidden . fst' $ execState (eval (runParserWithString parseBlsq p)) ([BlsqStr stdin],[BlsqStr stdin], M.fromList [])
-
+runProgram :: String -> String -> IO String
+runProgram p stdin = do
+ result <- execStateT (eval (runParserWithString parseBlsq (p))) ([BlsqStr stdin],[], M.fromList [])
+ return . unlines . map toDisplay . filter notHidden . fst' $ result
 
 main = do
  args <- getArgs
  case args of
    ["--file",file] -> do 
      prog <- readFile file
-     interact $ runProgram prog
+     cin <- getContents
+     cout <- runProgram prog cin
+     putStr cout
    _ -> do putStrLn $ "Invalid usage"
            putStrLn "  --file <path>           Read code from file (incl. STDIN)"
            putStrLn ""
